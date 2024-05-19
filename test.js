968286ce7db94393ea0f4f6582bca63219137cf6
AleenaAsh1527 <163378759+AleenaAsh1527@users.noreply.github.com>
2024-05-19 16:18:47 +0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+var students = ["Aleena","Eshmal","amna","Sumaira"];
+let searchstudents = students.find(findAleena)
+ students.splice(0,1,... ["zahid", "anwar",]);
+console.log(searchstudents);
+function(stdAleena){
+   if(std == "Aleena") return true;
+    else return false;
+ } 
+
+/*var students = ["Aleena","hirra","safa","rafia"];
+//student[5] = "Amna";
+students.sort();
+students.splice(0,1,... ["zahid", "anwar",]);
+console.log(...students);
+function findAleena(std){
+ if(std == "Aleena") return true;
+ else return false;
+}*/
